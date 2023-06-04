624a092018a4fe836a2f8dd92f80742ba3579b91
DamianKaczmarczyk <jokernick@o2.pl>
2023-06-04 21:41:23 +0200

    a

--- src/modal-coctact-us.js
+++ src/modal-coctact-us.js
@@ -2,6 +2,7 @@
   const refs = {
     openModalBtn1: document.querySelector('[contact-us-open]'),
     openModalBtn2: document.querySelector('[contact-us-open2]'),
+    openModalBtn3: document.querySelector('[contact-us-open3]'),
     closeModalBtn1: document.querySelector('[contact-us-close]'),
     closeModalBtn2: document.querySelector('[contact-us-close]'),
     modal1: document.querySelector('[contact-us-container]'),
