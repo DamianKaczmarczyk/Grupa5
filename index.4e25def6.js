14630b0a9e47bc2180a56979b82680554469ba5f
DamianKaczmarczyk <120136123+DamianKaczmarczyk@users.noreply.github.com>
2023-06-07 05:10:46 +0000

    Deploying to gh-pages from @ DamianKaczmarczyk/Grupa5@6aa5645ea9a51032c377625e8f4e365303e3a2b0 🚀

---
+++ index.4e25def6.js
@@ -0,0 +1,2 @@
+const swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,createElements:!0,slidesPerView:1,spaceBetween:10,breakpoints:{320:{slidesPerView:2,spaceBetween:16},768:{slidesPerView:2,spaceBetween:32},1280:{slidesPerView:3,spaceBetween:31}},navigation:{nextEl:".swiper__arrow--right",prevEl:".swiper__arrow--left"}});
+//# sourceMappingURL=index.4e25def6.js.map
